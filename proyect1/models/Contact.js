8c9131f23a2585bca8025aba64c0db053f7d6b03
joshua.burkhan <bai21030@byui.edu>
2025-03-06 20:58:55 -0500

    last

--- proyect1/models/Contact.js
+++ proyect1/models/Contact.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
+  _id: { type: String, required: true },
   email: { type: String, required: true },
+  username: { type: String, required: true },
+  name: { type: String, required: true },
+  ipAddress: { type: String, required: true },
 });
+
 
 module.exports = mongoose.model("Contact", contactSchema);
