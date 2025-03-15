595f441a961ab087c80f64622c10eb5a87aeb028
joshua.burkhan <bai21030@byui.edu>
2025-03-15 18:28:42 -0500

    host change

--- proyect1/swagger.js
+++ proyect1/swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: "API documentation for managing contacts",
     version: "1.0.2",
   },
-  host: "localhost:3000",
+  host: "https://cse-341-project1-ffak.onrender.com/",
   basePath: "/",
   schemes: ["http"],
 };
