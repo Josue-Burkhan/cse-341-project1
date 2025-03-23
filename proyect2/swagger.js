6c92c5b3c22a6426d8f14b97207d0a369c320245
joshua.burkhan <bai21030@byui.edu>
2025-03-22 19:15:10 -0500

    more change in the swagger.js

--- proyect2/swagger.js
+++ proyect2/swagger.js
@@ -4,18 +4,15 @@ const doc = {
   info: {
     title: "Wild Fantasy API",
     description: "API documentation for managing characters, factions, places, species, items, and more in the Wild Fantasy universe.",
-    version: "1.0.0",
-    contact: {
-      name: "Joshua Burkhan"
-    }
+    version: "1.0.1",
   },
   host: "cse-341-project1-1-wxiy.onrender.com",
-  servers: ["cse-341-project1-1-wxiy.onrender.com"],
   basePath: "/",
-  schemes: ["https"]
+  schemes: ["https", "http"],
 };
 
 const outputFile = "./docs/swagger.json";
 const endpointsFiles = ["./server.js"];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
+
